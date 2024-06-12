d0113ed80cb5717cee486006b3f19dff930b9ae7
RenanFumis <renan-oliveira19@hotmail.com>
2024-06-11 21:08:31 -0300

    Coloquei alguns validadores de informações no nome do heroi e do vilão

--- central_city/serializer.py
+++ central_city/serializer.py
@@ -1,29 +1,24 @@
 import re
 from rest_framework import serializers
 from .models import Heroi, Vilao
+from .validators import *
 
 class HeroiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Heroi
         fields = ['id', 'nome', 'codinome', 'poder', 'profissao', 'primeira_aparicao']
 
-    def validate_nome(self, nome):
-        pattern = r"^[A-Za-zÀ-ÖØ-öø-ÿ'\"\s]+$"
-
-        if not re.match(pattern, nome):
-            raise serializers.ValidationError('Nome deve conter apenas letras')
-        
-        return nome
+    def validate(self, data):
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':'O nome do herói deve conter apenas letras'})
+        return data
 
 class VilaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vilao
         fields = ['id', 'nome', 'codinome', 'poder', 'profissao', 'primeira_aparicao']
 
-    def validate_nome(self, nome):
-        pattern = r"^[A-Za-zÀ-ÖØ-öø-ÿ'\"\s]+$"
-
-        if not re.match(pattern, nome):
-            raise serializers.ValidationError('Nome deve conter apenas letras')
-        
-        return nome
+    def validate(self, data):
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':'O nome do vilão deve conter apenas letras'})
+        return data
